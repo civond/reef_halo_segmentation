dda9466e44321d17277482ea72939f3021fff61b
civond <doriansyeh@gmail.com>
2025-10-08 17:01:40 -0400

    track grid coords

--- utils/tile_img.py
+++ utils/tile_img.py
@@ -10,22 +10,26 @@ def tile_img(img, tile_size=512):
     pad_w = (tile_size - w % tile_size) % tile_size
     img_padded = np.pad(img, ((0, pad_h), (0, pad_w), (0, 0)), mode='constant')
     img_padded = img_padded[:, :, :3] # Ignore IR channel
-    print(img_padded.shape)
 
     # Tile the image
     tiles = []
-    for y in range(0, img_padded.shape[0], tile_size):
-        for x in range(0, img_padded.shape[1], tile_size):
+    n_rows = img_padded.shape[0] // tile_size
+    n_cols = img_padded.shape[1] // tile_size
+
+    print(f"\tPadded img shape: {img_padded.shape}")
+    print(f"\tN rows: {n_rows}")
+    print(f"\tN columns: {n_cols}")
+
+    for row_idx in range(n_rows):
+        for col_idx in range(n_cols):
+            y = row_idx * tile_size
+            x = col_idx * tile_size
             tile = img_padded[y:y+tile_size, x:x+tile_size]
 
-            # Append tiled image to tiles array with coordinates relative to the image
-            tiles.append((tile, (x, y)))
+            # Track grid coords relative to padded img
+            tiles.append((tile, (row_idx, col_idx)))
 
-    """for y in range(0, img_padded.shape[0], tile_size):
-        for x in range(0, img_padded.shape[1], tile_size):
-            tile = img_padded[y:y+tile_size, x:x+tile_size]
-            tiles.append(tile)
-    """
+    
     print(f"\tNum. tiles: {len(tiles)}")
     print(f"\tTile shape:{tiles[150][0].shape}")
     print(f"\tTile coords:{tiles[150][1]}")
