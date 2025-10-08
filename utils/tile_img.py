e164df6eafe3109c2a9249cbcbefa75f90c59f4c
civond <doriansyeh@gmail.com>
2025-10-08 16:54:29 -0400

    update tile fcn to include coords

--- utils/tile_img.py
+++ utils/tile_img.py
@@ -10,15 +10,24 @@ def tile_img(img, tile_size=512):
     pad_w = (tile_size - w % tile_size) % tile_size
     img_padded = np.pad(img, ((0, pad_h), (0, pad_w), (0, 0)), mode='constant')
     img_padded = img_padded[:, :, :3] # Ignore IR channel
+    print(img_padded.shape)
 
     # Tile the image
     tiles = []
     for y in range(0, img_padded.shape[0], tile_size):
         for x in range(0, img_padded.shape[1], tile_size):
             tile = img_padded[y:y+tile_size, x:x+tile_size]
-            tiles.append(tile)
 
+            # Append tiled image to tiles array with coordinates relative to the image
+            tiles.append((tile, (x, y)))
+
+    """for y in range(0, img_padded.shape[0], tile_size):
+        for x in range(0, img_padded.shape[1], tile_size):
+            tile = img_padded[y:y+tile_size, x:x+tile_size]
+            tiles.append(tile)
+    """
     print(f"\tNum. tiles: {len(tiles)}")
-    print(f"\tTile shape:{tiles[150].shape}")
+    print(f"\tTile shape:{tiles[150][0].shape}")
+    print(f"\tTile coords:{tiles[150][1]}")
 
     return tiles
