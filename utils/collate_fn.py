bde42804fb082d1c30f16875f6eb1fa006352fc0
civond <doriansyeh@gmail.com>
2025-11-12 16:07:53 -0500

    add comments

--- utils/collate_fn.py
+++ utils/collate_fn.py
@@ -1,2 +1,4 @@
+# Concats all dicts into one larger dict
+
 def collate_fn(batch):
     return tuple(zip(*batch))
--- utils/val_fn.py
+++ utils/val_fn.py
@@ -15,16 +15,18 @@ def val_fn(device, loader, model):
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
             # Forward pass
+            model.train() # Calculate loss
             with torch.amp.autocast('cuda'):
                 loss_dict = model(images, targets)
                 loss = sum(loss for loss in loss_dict.values())
+            model.eval() # Reset to eval
 
             total_loss += loss.item()
             loop.set_postfix(loss=loss.item())
         
     avg_loss = total_loss / len(loader)
     #avg_dice = total_dice / len(loader) 
-    
+
     # update tqdm loop
     loop.set_postfix(loss=loss.item())
 
