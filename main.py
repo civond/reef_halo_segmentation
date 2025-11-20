53c28af8b3f894a1a0575629fcc88a667df14778
civond <civond@users.noreply.github.com>
2025-11-20 10:37:50 -1000

    add try except to save

--- main.py
+++ main.py
@@ -54,8 +54,6 @@ def main():
         # Access TOML values
         train_dir = config["Paths"]["train_data_dir"]
         val_dir = config["Paths"]["val_data_dir"]
-        save_model_dir = config["Paths"]["save_model_dir"]
-        save_csv_dir = config["Paths"]["save_csv_dir"]
 
         learning_rate = config["Hyperparameters"]["learning_rate"]
         batch_size = config["Hyperparameters"]["batch_size"]
@@ -156,35 +154,47 @@ def main():
                 break
     
 
-    # Save model, history, figure
+    # Ensure directories exist and create paths
     now = datetime.now()
     timestamp = now.strftime("%Y_%m_%d_%H%M")
-    save_model_pth = os.path.join(save_model_dir, f"{timestamp}.pth")
-    save_csv_pth = os.path.join(save_csv_dir, f"{timestamp}.csv")
-    save_fig_pth = os.path.join("./figures/", f"{timestamp}.png")
 
-    print(f"\tSaving model at: {save_model_pth}")
-    torch.save(
-        best_model_weights, 
-        save_model_pth
-        )
-
-    print(f"\tSaving csv at: {save_csv_pth}")
-    df = pd.DataFrame({
-        "train_loss": train_loss_arr,
-        "val_loss": val_loss_arr
-    })
-
-    df.to_csv(
-        save_csv_pth, 
-        index=False
-    )
+    save_model_dir = "./model/"
+    save_csv_dir = "./logs/"
+    save_fig_dir = "./figures/"
     
-    print(f"\tSaving figure at: {save_fig_pth}")
-    create_fig(
-        df, 
-        save_fig_pth
-    )
+    try:
+        os.makedirs(save_model_dir, exist_ok=True)
+        os.makedirs(save_csv_dir, exist_ok=True)
+        os.makedirs(save_fig_dir, exist_ok=True)
 
+        save_model_pth = os.path.join(save_model_dir, f"{timestamp}.pth")
+        save_csv_pth = os.path.join(save_csv_dir, f"{timestamp}.csv")
+        save_fig_pth = os.path.join(save_fig_dir, f"{timestamp}.png")
+
+        print(f"\tSaving model at: {save_model_pth}")
+        torch.save(
+            best_model_weights, 
+            save_model_pth
+            )
+
+        print(f"\tSaving csv at: {save_csv_pth}")
+        df = pd.DataFrame({
+            "train_loss": train_loss_arr,
+            "val_loss": val_loss_arr
+        })
+
+        df.to_csv(
+            save_csv_pth, 
+            index=False
+        )
+        
+        print(f"\tSaving figure at: {save_fig_pth}")
+        create_fig(
+            df, 
+            save_fig_pth
+        )
+    except Exception as e:
+        print(f"Error during saving: {e}")
+        
 if __name__ == "__main__":
     main()
